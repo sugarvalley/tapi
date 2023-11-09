e854610d7ad2f10e3b1930e957a3e4c70403351f
sugarvalley <cqrkowa@gmail.com>
2023-11-09 16:29:51 +0100

    09/11

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import {getCourse, getStudent} from "../dataGen/generator.js";
 import * as protoLoader from "@grpc/proto-loader";
 import * as grpc from "@grpc/grpc-js";
@@ -12,8 +12,17 @@ const client = new scheduleProto.ScheduleService('127.0.0.1:9090', grpc.ChannelC
 
 export const studentsRouter = express.Router();
 
+// studentsRouter.get('/:id', (req, res) => {
+//     res.send(getStudent(req.params.id));
+// }).get('/:id/schedule', (req, res) => {
+//     res.send(getCourse(req.params.id));
+// });
+
 studentsRouter.get('/:id', (req, res) => {
-    res.send(getStudent(req.params.id));
-}).get('/:id/schedule', (req, res) => {
-    res.send(getCourse(req.params.id));
-});
+    client.getStudent({studentId: "1"}, (err, response) => {
+        if (err != null) {
+            console.error(err);
+        }
+    })
+    res.send(response)
+})
