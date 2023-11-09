2d6ca086fbc538ff6d606de097a7ca2ce54684f7
sugarvalley <cqrkowa@gmail.com>
2023-11-09 16:25:45 +0100

    09/11

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { createServer } from "node:http";
+import * as protoLoader from "@grpc/proto-loader";
+import * as grpc from "@grpc/grpc-js";
 import cors from "cors";
 // import {io} from "./websocket/socket.js";
 import {studentsRouter} from "./routes/students.js"
@@ -7,7 +9,7 @@ import {coursesRouter} from "./routes/courses.js"
 
 
 export const app = express();
-export const server = createServer(app);
+export const rest = createServer(app);
 
 app.use(cors({
     origin: '*'
@@ -16,8 +18,28 @@ app.use(cors({
 app.use('/students', studentsRouter);
 app.use('/courses', coursesRouter);
 
-server.listen(3000, () => {
-    // io.listen(server, {
+const packageDefinition = protoLoader.loadSync('server/schedule.proto');
+const scheduleProto = grpc.loadPackageDefinition(packageDefinition);
+
+const server = new grpc.Server();
+
+server.bindAsync("127.0.0.1:9090", grpc.ServerCredentials.createInsecure(), (err, port) => {
+    if(err != null) {
+        return console.error(err);
+    }
+    console.log(`gRPC listening on ${port}`);
+    server.start();
+});
+
+server.addService(scheduleProto.ScheduleService.service, {
+    GetStudent: (req, res) => {
+        console.log(req.request);
+        res({name: "", surname: ""});
+    }
+})
+
+rest.listen(3000, () => {
+    // io.listen(rest, {
     //     cors: {
     //         origin: 'http://localhost:5173',
     //     }
--- server/routes/students.js
+++ server/routes/students.js
@@ -1,5 +1,14 @@
 import express from "express";
 import {getCourse, getStudent} from "../dataGen/generator.js";
+import * as protoLoader from "@grpc/proto-loader";
+import * as grpc from "@grpc/grpc-js";
+
+const packageDefinition = protoLoader.loadSync('server/schedule.proto');
+const scheduleProto = grpc.loadPackageDefinition(packageDefinition);
+
+protoLoader.loadSync('server/schedule.proto');
+
+const client = new scheduleProto.ScheduleService('127.0.0.1:9090', grpc.ChannelCredentials.createInsecure());
 
 export const studentsRouter = express.Router();
 
